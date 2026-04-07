bfe09cbd7d56c8bd017e07834c506d4e6937937b
tojo <chanon.chonv@gmail.com>
2026-04-07 16:15:09 +0700

    Create env-config.js

---
+++ env-config.js
@@ -0,0 +1,4 @@
+window.ENV = {
+    URL: 'https://itjzvcbuzyywgwdfjonp.supabase.co',
+    KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0anp2Y2J1enl5d2d3ZGZqb25wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NjYxMjQsImV4cCI6MjA5MDQ0MjEyNH0.U0wkJYwL4elzcAazlqm9uq4tk9JPOUD6CoIND61lLO0'
+};
